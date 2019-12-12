bea599367920acca4ecec951c8cba1045093e1c0
Xuyang Wang <davidwang@didiglobal.com>
2019-12-12 13:55:27 -0800

    Test Component Rendering

--- counter-app/src/App.test.js
+++ counter-app/src/App.test.js
@@ -4,10 +4,15 @@ import App from "./App";
 
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdaptor from "enzyme-adapter-react-16";
+import { exportAllDeclaration } from "@babel/types";
 
 Enzyme.configure({ adapter: new EnzymeAdaptor() });
 
-test("Render without Errors", () => {});
+test("Render without Errors", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
 
 test("Render Increase Btn", () => {});
 

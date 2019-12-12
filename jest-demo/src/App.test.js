ed7ac930fe85916249237a4a7baf2054403bab62
Xuyang Wang <davidwang@didiglobal.com>
2019-12-12 11:41:56 -0800

    setup & test enzyme

--- jest-demo/src/App.test.js
+++ jest-demo/src/App.test.js
@@ -1,9 +1,13 @@
 import React from "react";
 // import { render } from '@testing-library/react';
-import Enzyme from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders without crashing", () => {});
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  console.log(wrapper.debug());
+  expect(wrapper).toBeTruthy();
+});

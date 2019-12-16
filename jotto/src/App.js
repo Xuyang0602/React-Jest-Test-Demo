a2010b6b40f9913cbf405ea5e9a70a1ceb6a1d96
Xuyang Wang <davidwang@didiglobal.com>
2019-12-16 11:15:33 -0800

    Setup Redux

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Jotty</h1>
+        <h1>Jotto</h1>
         <Congrats success={true} />
         <GuessWords
           guessWords={[{ guessWord: "train", letterMatchCount: 3 }]}
